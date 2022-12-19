8652f94639d1819e4bb40f0b22e6a3714669e421
Pranoy12 <pranoyderil23@gmail.com>
2022-12-19 16:24:43 +0530

    Changes to be committed: 	modified:   register.php

--- register.php
+++ register.php
@@ -2,14 +2,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+   
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register Page</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <style type="text/css" >
+        .form-signup{
+        margin: 0 auto;
+        max-width: 400px;
+        text-align: left;
+        padding-top: 100px;
+        }
+        body{
+            
+            background-color: black;
+        }
+        input {
+            float:right;
+            display:inline-block;
+        }
+        h1{
+          text-color: blue;  
+        }
+        .register_button{
+            background-color: green;
+        }
+        </style>
 </head>
 <body>
-    <form action="register.php" method="post">
+    <form action="register.php" method="post" class="form-signup">
+        <h1>REGISTER</h1>
         User Name: <input type="text" name="user">
         <br>
         <br>
@@ -26,7 +50,7 @@
         <br>
         <br>
         <br>
-        <input type="submit" name="reg" value="REGISTER">
+        <input type="submit" name="reg" value="REGISTER" class="register_button">
         <p> Already a member? <a href="login.php">Login</a>
     </form>
 </body>
